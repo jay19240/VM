051b458712b2a0c08532e4e118b0bce5a6222a99
jay24120 <aliyah.raijin@gmail.com>
2026-09-18 10:19:54 +0300

    update

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      name: "api-backend",
+      script: "./server.js",
+      env: {
+        PORT: 3000
+      }
+    },
+    {
+      name: "web-frontend",
+      script: "npm",
+      args: "run dev",
+      cwd: "./engine",
+      watch: true,
+      ignore_watch: ["node_modules", ".next", "dist"],
+      env: {
+        PORT: 4000
+      }
+    }
+  ]
+};
